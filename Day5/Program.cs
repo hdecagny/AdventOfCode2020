9e2c23c34fd0b481af9abf3d7c28b884fc8baa90
Hcagny <Hcagny@PCD5268>
2020-12-06 17:53:48 +0100

    Refining Day 5

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -11,23 +11,23 @@ namespace Day5
             var data = System.IO.File
                 .ReadAllLines(@"C:\Users\Hcagny\source\repos\AdventOfCode2020\Day5\Data.txt")
                 .ToList();
+            var iDList = data.Select(ConvertToID).ToList();
 
-            var rowSeatPairList = data.Select(ConvertToNumbers).ToList(); //Converting those data into lists of two numbers : row and seat
+            var result1bis = SolvePuzzle1(iDList);
 
-            var result1 = SolvePuzzle1(rowSeatPairList);
+            var result2bis = SolvePuzzle2(iDList);
 
-            var result2 = SolvePuzzle2(rowSeatPairList);
 
-            Console.WriteLine(result1);
-            Console.WriteLine(result2);
+            Console.WriteLine(result1bis);
+            Console.WriteLine(result2bis);
         }
 
-        public static int[] ConvertToNumbers(string input)
+        public static int ConvertToID(string input)
         {
             var binaryInput = input.Replace('B', '1')
-                                          .Replace('F', '0')
-                                          .Replace('L','0')
-                                          .Replace('R','1');
+                .Replace('F', '0')
+                .Replace('L', '0')
+                .Replace('R', '1');
 
             var binaryRow = binaryInput.Substring(0, 7);
             var binarySeat = binaryInput.Substring(7, 3);
@@ -35,25 +35,24 @@ namespace Day5
             var rowNumber = Convert.ToInt32(binaryRow, 2);
             var seatNumber = Convert.ToInt32(binarySeat, 2);
 
-            return new []{ rowNumber  , seatNumber };
-
+            return rowNumber * 8 + seatNumber;
         }
 
-        public static int SolvePuzzle1(IList<int[]> input)
+        public static int SolvePuzzle1(IList<int> input)
         {
-            return input.Select(pair => pair[0] * 8 + pair[1]).Max();
+            return input.Max();
         }
 
-        public static int SolvePuzzle2(IList<int[]> input) 
+        public static int SolvePuzzle2(IList<int> input)
         {
-            var ticketsForIncompleteRow = input.GroupBy(i => i[0])
-                .First(i => i.Count<int[]>() == 7); //All the complete rows should have 8 tickets
+            var iDmin = input.Min();
+            var iDMax = input.Max();
 
-            var missingRow = ticketsForIncompleteRow.Key;
-            var incompleteSeat = 7 * 8 / 2 - ticketsForIncompleteRow.Select(i => i[1]).ToList().Sum(); // 0+1+..+8 = 7*8/2 If I sum all the tickets I have, I should arrive to the missing one
+            var result = (iDMax * (iDMax + 1) - iDmin * (iDmin - 1)) / 2 - input.Sum();
 
-            return missingRow * 8 + incompleteSeat;
-
+            return result;
         }
+
+
     }
 }
