fa68f9f6ae66a1a41259c22ea3ee2241413d10d0
Hcagny <Hcagny@PCD5268>
2020-12-11 18:02:41 +0100

    Day10 (for once a bit elegant)

---
+++ Day10/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = System.IO.File
+                .ReadAllLines(@"C:\Users\Hcagny\source\repos\AdventOfCode2020\Day10\Data.txt")
+                .Select(int.Parse)
+                .ToList();
+
+            data.Sort();
+
+            var result1 = SolvePuzzle1(data);
+            Console.WriteLine(result1);
+
+            var result2 = SolvePuzzle2(data);
+            Console.WriteLine(result2);
+        }
+
+        public static int SolvePuzzle1(List<int> data)
+        {
+            var diff1 = 0;
+            var diff3 = 0;
+
+            var currentJoltage = 0;
+
+            foreach (var adapter in data)
+            {
+                switch (adapter - currentJoltage)
+                {
+                    case (1):
+                        diff1++;
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        diff3++;
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+
+                currentJoltage = adapter;
+            }
+
+            diff3++;
+
+            return diff1 * diff3;
+        }
+
+        public static long SolvePuzzle2(List<int> data)
+        {
+            var numberOfCombinationToReachGivenVoltage = new Dictionary<int, long> {{0, 1}, {-1, 0}, {-2, 0}};
+
+            foreach (var adapter in data)
+            {
+                var test1 = numberOfCombinationToReachGivenVoltage.ContainsKey(adapter - 1) ? numberOfCombinationToReachGivenVoltage[adapter - 1] : 0;
+                var test2 = numberOfCombinationToReachGivenVoltage.ContainsKey(adapter - 2) ? numberOfCombinationToReachGivenVoltage[adapter - 2] : 0;
+                var test3 = numberOfCombinationToReachGivenVoltage.ContainsKey(adapter - 3) ? numberOfCombinationToReachGivenVoltage[adapter - 3] : 0;
+
+                var numberofCombination = test1 + test2 + test3;
+
+                numberOfCombinationToReachGivenVoltage.Add(adapter,numberofCombination);
+            }
+
+            var last = data.Last();
+            return numberOfCombinationToReachGivenVoltage[last];
+        }
+    }
+}
